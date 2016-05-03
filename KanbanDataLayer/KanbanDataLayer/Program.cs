4ccddab03bbc8ba6c04d5dde39cd7291196da1f2
Rebecca Mooney <astrakiseki@gmail.com>
2016-05-03 06:17:55 -0700

    Code First and whitespace removal.  Should also remove the practice folder.

---
+++ KanbanDataLayer/KanbanDataLayer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanDataLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                using (var db = new KanbanEntities())
+                {
+                    foreach (var list in db.Lists)
+                    {
+
+                        Console.WriteLine(list.Name + "\n");
+                        foreach (var card in list.Cards)
+                        {
+                            Console.WriteLine("\t" + card.Text);
+                        }
+                        Console.WriteLine("\n");
+                    }
+                    Console.ReadLine();
+                    Console.WriteLine("Would you like to add a card?");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
